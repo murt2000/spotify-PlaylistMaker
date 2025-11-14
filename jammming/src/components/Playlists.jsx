4c7f4e7c3b9096933e97becd40344835b2da53ca
murt2000 <sivertmuren@gmail.com>
2025-11-14 19:35:17 +0100

    playlist logic

--- jammming/src/components/Playlists.jsx
+++ jammming/src/components/Playlists.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Tracklist from "./Tracklist";
 import Track from "./Track";
 
@@ -6,23 +6,117 @@ import Track from "./Track";
 
 function Playlists() {
 
-    const sampleTracks = [
-        {id: 1, name: 'What you know', artist: 'Two door cinema club', image:'cat.thing'},
-        {id: 2, name: 'chinatown Blues', artist:'ODDEEO, karmaWears White Tears', image:'sunset.png'}
-    ]
+    const [tracklists, setTracklists] = useState([
+        {id: 1,
+        name: 'repeat',
+        expanded: true,
+        editingName: false,
+        tracks:[
+            {id: 101, name: 'What you know', artist: 'Two door cinema club', image:'cat.thing'},
+            {id: 102, name: 'chinatown Blues', artist:'ODDEEO, karmaWears White Tears', image:'sunset.png'}
+        ]
+        },
+        {
+            id:2,
+            name: 'metal',
+            expanded: false,
+            editingName: false,
+            tracks:[
+                { id:201, name:'Son of a wolf', artist:'Powerwolf', image: 'wolf-bishop.png' },
+                {id:202, name: 'Army of the Night', artist:'Powerwolf', image: 'wolf-pagan.png'}
+            ]
+        }
 
+    ])
+
+    function toggleTracklist(id) {
+        setTracklists(prev =>
+            prev.map(tl =>
+                tl.id === id
+                ? {...tl, expanded: !tl.expanded}
+                : {...tl, expanded: false }
+            )
+
+        );
+
+    }
+
+    function addTracklist() {
+        const newList = {
+            id: Date.now(),
+            name: `new Tracklist ${tracklists.length + 1}`,
+            expanded: true,
+            editingName: false,
+            tracks: []
+        };
+        setTracklists(prev =>
+            prev.map(tl => ({...tl, expanded: false })).concat(newList)
+        )
+        
+    }
+
+    function startEditing(id) {
+        setTracklists(prev =>
+            prev.map(tl =>
+                tl.id === id
+                ? {...tl, editingName: true}
+                : tl
+            )
+        )
+        
+    }
+    function saveName(id, newName){
+        setTracklists(prev =>
+            prev.map(tl =>
+                tl.id === id
+                ?{...tl, name: newName, editingName: false}
+                : tl
+            )
+        )
+    }
     return(
         <div id='Playlists'>
             <h2>Your playlists</h2>
-            <Tracklist
-                tracks={sampleTracks}
+            <button onClick={addTracklist}>+</button>
+
+
+            {tracklists.map(tl => (
+                <div key={tl.id}>
+
+                    {!tl.editingName ? ( 
+
+                    <h3 
+                        onClick={() => toggleTracklist(tl.id)}
+                        onDoubleClick={() =>startEditing(tl.id)}
+                        style={{cursor: "pointer", userSelect: 'none'}}    
+                    >
+                        {tl.name}
+                    </h3>) :(
+                        <input
+                        type='text'
+                        autoFocus
+                        defaultValue={tl.name}
+                        onBlur={(e) => saveName(tl.id, e.target.value)}
+                        onKeyDown={(e) => {
+                            if(e.key === 'Enter'){
+                                saveName(tl.id, e.target.value)
+                            }
+                        }}
+                        />
+                    )}   
+
+                    {tl.expanded && (
+
+                        <Tracklist
+                tracks={tl.tracks}
                 isInPlaylist={true}
                 addTrack={(name) => console.log(`adding ${name}`)}
                 rmvTrack={(name) => console.log(`removeing ${name}`)}
-                
                 />
-                
+            )}
+            </div>
+            ))}
         </div>
     );
 }
-export default Playlists
+export default Playlists;
