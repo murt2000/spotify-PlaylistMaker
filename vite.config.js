d21a7d811ad2b2e902ab4e6f210e6da393671d98
murt2000 <sivertmuren@gmail.com>
2026-01-17 00:13:45 +0100

    added dist for pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   base: "/spotify-PlaylistMaker/",
   server: {
-    host: "127.0.0.1", // <-- important
-    port: 5173,        // <-- important
+    host: "127.0.0.1",
+    port: 5173,
   },
 });
