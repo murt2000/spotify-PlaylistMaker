3210002d6834004336b9b18343968c121c78975d
murt2000 <sivertmuren@gmail.com>
2026-01-17 01:04:30 +0100

    icons readded

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ function Header({ profileData, loadingProfile, profileError, toggleTheme, theme,
             <div id="titleAndButton">
                 <h1>Spotify Playlist Maker</h1>
                 <button id="hbutton" onClick={() => toggleTheme()}>
-                    <img className="day-night" src={theme === "dark" ? "dist/day-mode.png" : "dist/moon.png"} />
+                    <img className="day-night" src={theme === "dark" ? "assets/day-mode.png" : "assets/moon.png"} />
                 </button>
             </div>
             <ProfileInfo profileData={profileData} loadingProfile={loadingProfile} profileError={profileError} handleLogout={handleLogout} />
