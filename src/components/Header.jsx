7cd1c084ff831ef43016a61a95940f64344e9a12
murt2000 <sivertmuren@gmail.com>
2026-01-17 01:32:52 +0100

    relative filepaths added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./styleComponents/Header.css";
 import ProfileInfo from "./ProfileInfo.jsx";
+import dayIcon from "./assets/day-mode.png";
+import moonIcon from "./assets/moon.png";
 
 /*
 Header continaing the website title and a night day button.
@@ -13,7 +15,7 @@ function Header({ profileData, loadingProfile, profileError, toggleTheme, theme,
             <div id="titleAndButton">
                 <h1>Spotify Playlist Maker</h1>
                 <button id="hbutton" onClick={() => toggleTheme()}>
-                    <img className="day-night" src={theme === "dark" ? "src\assets\day-mode.png" : "src\assets\moon.png"} />
+                    <img className="day-night" src={theme === "dark" ? dayIcon : moonIcon} />
                 </button>
             </div>
             <ProfileInfo profileData={profileData} loadingProfile={loadingProfile} profileError={profileError} handleLogout={handleLogout} />
